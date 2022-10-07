d4a4944c576aa3faac10278637ac3ed3487989ad
Victor Prado <prado.victor1005@gmail.com>
2022-10-07 19:25:40 -0300

    ajustando tela home

--- src/Components/CardVagasEmAberto/index.js
+++ src/Components/CardVagasEmAberto/index.js
@@ -1,58 +1,85 @@
 import { useEffect, useState } from 'react';
 
-import { CardVagas } from './style';
+import { Container, CardVagas } from './style';
 
 import VagasService from '../../services/VagasService';
+import CandidatoService from '../../services/CandidatoService';
+
+import like from '../../assets/images/like.png';
+import dislike from '../../assets/images/dislike.png';
 
 export default function CardVagasEmAberto() {
-  // const [firstElement, setFirstElement] = useState([]);
+  const UserId = '8a79694c-9a53-483f-b5c7-e4748573ecee';
   const [vagas, setVagas] = useState([]);
 
   useEffect(() => {
     (async () => {
       try {
         const vagaList = await VagasService.getVagas();
-        vagaList.length = 1;
         setVagas(vagaList);
-        // console.log('first element', firstElement);
       } catch (error) {
         console.log(error.message);
       }
     })();
   }, []);
 
-  function handleCandidatar() {
-    setVagas(vagas.slice(1));
+  function handleDislikeVaga(vagaClick) {
+    setVagas((prevState) => prevState.filter(
+      (vaga) => vaga.id !== vagaClick.id,
+    ));
+  }
+
+  async function handleCandidatar(vagaClick) {
+    try {
+      const Candidatura = {
+        id_vaga: vagaClick.id,
+        id_candidato: UserId,
+      };
+
+      await CandidatoService.createCandidatura(Candidatura);
+
+      setVagas((prevState) => prevState.filter(
+        (vaga) => vaga.id !== vagaClick.id,
+      ));
+    } catch {
+      alert('Não foi possível se candidatar a essa vaga. Tente Novamente.');
+    }
   }
 
   return (
-    <>
+    <Container>
       {vagas.map((vaga) => (
         <CardVagas key={vaga.id}>
           <div className="title-card">
             <span>Vagas em Aberto</span>
           </div>
-          <div>
+          <div className="container-card-info">
             <div className="container-info-vaga">
               <span>{vaga.nome}</span>
             </div>
-            <span>Requisito:</span>
+            <div className="subtitle-card">
+              <span>Requisito:</span>
+            </div>
             <div className="container-info-vaga">
               <span>{vaga.requisitos}</span>
             </div>
-            <span>Descrição:</span>
+            <div className="subtitle-card">
+              <span>Descrição:</span>
+            </div>
             <div className="container-info-vaga">
               <span>{vaga.descricao}</span>
             </div>
           </div>
           <div className="button-area">
-            <button type="button">Não</button>
-            <button type="button" onClick={handleCandidatar}>
-              Sim
+            <button type="button" onClick={() => handleDislikeVaga(vaga)}>
+              <img src={dislike} alt="" />
+            </button>
+            <button type="button" onClick={() => handleCandidatar(vaga)}>
+              <img src={like} alt="" />
             </button>
           </div>
         </CardVagas>
       ))}
-    </>
+    </Container>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,28 @@
 import { useEffect, useState } from 'react';
 
-import { PontosContainer, TitleContainer, ContainerCards } from './style';
+import {
+  PontosContainer,
+  TitleContainer,
+  ContainerCards,
+  ContainerVagas,
+  CardVaga, CardVagas,
+} from './style';
 
 // import { Context } from '../../Context/AuthContext';
 import UserService from '../../services/UserService';
-// import CandidatoService from '../../services/CandidatoService';
+import CandidatoService from '../../services/CandidatoService';
+import VagasService from '../../services/VagasService';
 
 import imgPontos from '../../assets/images/ponto.png';
+import like from '../../assets/images/like.png';
+import dislike from '../../assets/images/dislike.png';
 
 import Header from '../../Components/Header';
-import CardVagasEmAberto from '../../Components/CardVagasEmAberto';
-import CardCandidatoVagas from '../../Components/CardCandidatoVagas';
 
 export default function Home() {
   const [userData, setUserData] = useState('');
+  const [candidaturas, setCandidaturas] = useState([]);
+  const [vagas, setVagas] = useState([]);
   // const { authenticated } = useContext(Context);
   const UserId = '8a79694c-9a53-483f-b5c7-e4748573ecee';
 
@@ -27,13 +36,42 @@ export default function Home() {
       try {
         const user = await UserService.getUserById(UserId);
         setUserData(user);
-        // const vagasCandidato = await CandidatoService.getVagasByCandidatoId(UserId);
-        // console.log(vagasCandidato);
+        const vagaList = await VagasService.getVagas();
+        const vagasCandidato = await CandidatoService.getVagasByCandidatoId(
+          UserId,
+        );
+        setCandidaturas(vagasCandidato);
+        setVagas(vagaList);
+        for (let index = 0; index < vagasCandidato.length; index++) {
+          const element = vagasCandidato[index];
+          setVagas((prevState) => prevState.filter((vaga) => vaga.id !== element.id));
+        }
       } catch (error) {
         console.log(error.message);
       }
     })();
   }, []);
+
+  function handleDislikeVaga(vagaClick) {
+    setVagas((prevState) => prevState.filter((vaga) => vaga.id !== vagaClick.id));
+  }
+
+  async function handleCandidatar(vagaClick) {
+    try {
+      const Candidatura = {
+        id_vaga: vagaClick.id,
+        id_candidato: UserId,
+      };
+
+      await CandidatoService.createCandidatura(Candidatura);
+
+      setCandidaturas(await CandidatoService.getVagasByCandidatoId(UserId));
+
+      setVagas((prevState) => prevState.filter((vaga) => vaga.id !== vagaClick.id));
+    } catch {
+      alert('Não foi possível se candidatar a essa vaga. Tente Novamente.');
+    }
+  }
 
   return (
     <>
@@ -49,11 +87,72 @@ export default function Home() {
           {userData.nome}
           !
         </h3>
-        <span>Visualize e encontre vagas de acordo com seu plano de carreira</span>
+        <span>
+          Visualize e encontre vagas de acordo com seu plano de carreira
+        </span>
       </TitleContainer>
       <ContainerCards>
-        <CardVagasEmAberto />
-        <CardCandidatoVagas />
+        <div className="card-testes">
+          <h1>Alo</h1>
+        </div>
+        <div className="cardVagas">
+          <>
+            {vagas.map((vaga) => (
+              <CardVagas key={vaga.id}>
+                <div className="title-card">
+                  <span>Vagas em Aberto</span>
+                </div>
+                <div className="container-card-info">
+                  <div className="container-info-vaga">
+                    <span>{vaga.nome}</span>
+                  </div>
+                  <div className="subtitle-card">
+                    <span>Requisito:</span>
+                  </div>
+                  <div className="container-info-vaga">
+                    <span>{vaga.requisitos}</span>
+                  </div>
+                  <div className="subtitle-card">
+                    <span>Descrição:</span>
+                  </div>
+                  <div className="container-info-vaga">
+                    <span>{vaga.descricao}</span>
+                  </div>
+                </div>
+                <div className="button-area">
+                  <button type="button" onClick={() => handleDislikeVaga(vaga)}>
+                    <img src={dislike} alt="" />
+                  </button>
+                  <button type="button" onClick={() => handleCandidatar(vaga)}>
+                    <img src={like} alt="" />
+                  </button>
+                </div>
+              </CardVagas>
+            ))}
+          </>
+        </div>
+        <div>
+          <ContainerVagas>
+            <div className="title-card">
+              <span>Histórico de Candidatura</span>
+            </div>
+            <span className="subtitle-card">
+              A cada candidatura você ganha +5 JobPoints e +10XP
+            </span>
+            {candidaturas.map((vaga) => (
+              <CardVaga key={vaga.id}>
+                <div className="container-vaga">
+                  <div className="container-info-vaga">
+                    <span>{vaga.nome}</span>
+                  </div>
+                  <div>
+                    <button type="button">Exibir Detalhes</button>
+                  </div>
+                </div>
+              </CardVaga>
+            ))}
+          </ContainerVagas>
+        </div>
       </ContainerCards>
     </>
   );
